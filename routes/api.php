0b26048e44360b4f85ad9e7ddf48a8ebb670bb7b
Melan Eka Putri <melanputrimelan@gmail.com>
2019-03-15 18:09:32 +0700

    #3 - Laravel + NuxtJS authentication- Middleware and -me endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,10 @@ Route::group(['prefix' => '/auth',['middleware' => 'throttle:20,5']], function()
     Route::post('/register','Auth\RegisterController@register');
     Route::post('/login','Auth\LoginController@login');
 });
+
+Route::group(['middleware' => 'jwt.auth'], function(){
+    Route::get('/me', 'MeController@index');
+
+    Route::get('/auth/logout', 'MeController@logout');
+
+});
